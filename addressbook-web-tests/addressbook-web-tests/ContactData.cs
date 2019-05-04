5aa9ca8040711073346cb1743489d7eccb529d1c
Владимир <DELL@DESKTOP-5VBPB33>
2019-05-04 13:51:21 +0300

    Задание 4 "Создание нового контакта"

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace addressbook_web_tests
+{
+    class ContactData
+{
+        private string firstname;
+        private string lastname;
+
+        public ContactData(string firstname)
+        {
+            this.firstname = firstname;
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+    }
+}
