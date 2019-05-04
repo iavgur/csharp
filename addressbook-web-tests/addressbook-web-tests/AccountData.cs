06e0dc46076856d713806193b005c85e684b74aa
Владимир <DELL@DESKTOP-5VBPB33>
2019-05-04 12:02:28 +0300

    задание 3

---
+++ addressbook-web-tests/addressbook-web-tests/AccountData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace addressbook_web_tests
+{
+    class AccountData
+{
+    private string username;
+    private string password;
+
+    public AccountData(string usernam, string password)
+    {
+        this.username = usernam;
+        this.password = password;
+    }
+
+    public string Username
+    {
+        get
+        {
+            return username;
+        }
+        set
+        {
+            username = value;
+        }
+    }
+
+    public string Password
+    {
+        get
+        {
+            return password;
+        }
+        set
+        {
+            password = value;
+        }
+    }
+}
+}
